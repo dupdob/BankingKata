0fabb3f8afb7d87de466b7f39919226f3fca518a
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2018-06-05 23:12:39 +0200

    First commit

---
+++ BankingKata/Account.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace BankingKata
+{
+    internal class Account
+    {
+        private IList<Operation> operations = new List<Operation>();
+        private bool depositDone = false;
+
+        public void Deposit(double amount, String date)
+        {
+            this.operations.Add(new Operation(amount, date));
+            this.depositDone = true;
+        }
+
+        public void Withdraw(double amount, String date)
+        {
+            this.operations.Add(new Operation(-amount, date));
+            this.depositDone = true;
+        }
+
+        public void PrintStatement()
+        {
+            Console.WriteLine("DATE | AMOUNT | BALANCE");
+            if (this.depositDone)
+            {
+                var report = new StringBuilder();
+                double balance = 0;
+                foreach (var operation in operations)
+                {
+                    balance += operation.Amount;
+                    if (operation.Amount>=0)
+                    {
+                        report.Insert(0, 
+                        string.Format("{0} | {1:F2} | {2:F2}"+Environment.NewLine, 
+                        operation.Date, 
+                        operation.Amount, 
+                        balance));
+                    }
+                    else
+                    {
+                        // TODO: confirm with PO if this is expected format
+                        report.Insert(0, 
+                            string.Format("{0} | {1} | {2:F2}"+Environment.NewLine, 
+                                operation.Date, 
+                                operation.Amount, 
+                                balance));
+                    }
+                }
+
+                Console.Write(report.ToString());
+            }
+        }
+
+        struct Operation
+        {
+            public double Amount;
+            public string Date;
+
+            public Operation(double amount, string date)
+            {
+                Amount = amount;
+                Date = date;
+            }
+        }
+    }
+}
---
+++ BankingKata/Tests/AccountShould.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace BankingKata.Tests
+{
+    [TestFixture]
+    class AccountShould
+    {
+        [Test]
+        public void
+            SupportDemoUseCase()
+        {
+            // Given
+            var myAccount = new Account();
+
+            // When
+            myAccount.Deposit(1000, "01/04/2014");
+            myAccount.Withdraw(100, "02/04/2014");
+            myAccount.Deposit(500, "10/04/2014");
+            var recorder = new ConsoleRecorder();
+            using (recorder)
+            {
+                myAccount.PrintStatement();
+            }
+
+            // Then
+            // console should display
+            Check.That(recorder.Display).AsLines().ContainsExactly(
+                "DATE | AMOUNT | BALANCE",
+                "10/04/2014 | 500,00 | 1400,00",
+                "02/04/2014 | -100 | 900,00",
+                "01/04/2014 | 1000,00 | 1000,00");
+
+        }
+
+        [Test]
+        public void
+            SupportSimpleDeposit()
+        {
+            // Given
+            var myAccount = new Account();
+
+            // When
+            myAccount.Deposit(1000, "01/04/2014");
+            var recorder = new ConsoleRecorder();
+            using (recorder)
+            {
+                myAccount.PrintStatement();
+            }
+
+            // Then
+            // console should display
+            Check.That(recorder.Display).AsLines().ContainsExactly(
+                "DATE | AMOUNT | BALANCE",
+                "01/04/2014 | 1000,00 | 1000,00");
+        }
+
+
+        [Test]
+        public void
+            PrintHeadersWhenNoOperation()
+        {
+            // given
+            var myAccount = new Account();
+
+            var recorder = new ConsoleRecorder();
+            using (recorder)
+            {
+                // when
+                myAccount.PrintStatement();                
+            }
+
+            // Then console should display
+            // DATE | AMOUNT | BALANCE
+
+            Check.That(recorder.Display).AsLines().ContainsExactly("DATE | AMOUNT | BALANCE");
+        }
+    }
+
+    // capture console display with using pattern
+    internal class ConsoleRecorder : IDisposable
+    {
+        private readonly TextWriter oldOutStream;
+        private readonly StringWriter recorder;
+
+        public string Display => recorder.ToString();
+
+        public ConsoleRecorder()
+        {
+            this.oldOutStream = Console.Out;
+            this.recorder = new StringWriter();
+            Console.SetOut(recorder);
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(this.oldOutStream);
+        }
+    }
+}
